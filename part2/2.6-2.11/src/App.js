9b8302d7e41c89c0de9579a5351864215b5ed172
rder96 <rder@wisc.edu>
2020-12-02 13:11:29 +0800

    rearranged 2.6-2.10 to include 2.11

--- part2/2.6-2.11/src/App.js
+++ part2/2.6-2.11/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ( {name, number} ) => { 
   return (
@@ -37,12 +38,7 @@ const PersonForm = ( {addNewPerson, newName, handleNameChange, newNumber, handle
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newSearch, setNewSearch ] = useState('')
@@ -68,6 +64,18 @@ const App = () => {
 
   const handleNewSearch = (event) => 
     setNewSearch(event.target.value)   
+
+  useEffect(() => {
+    // console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        // console.log(response.data)
+        setPersons(response.data)
+      })
+  }, [])
+  
+  // console.log('render', persons.length, 'persons')
 
   return (
     <div>
