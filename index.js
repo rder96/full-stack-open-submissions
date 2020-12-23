41699b890035ba7867bfb68fc34a922ddde585c0
rder96 <rder@wisc.edu>
2020-12-23 15:25:40 +0800

    uibuild

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.get('/', (request, response) => {
     response.send('hello world')
 })
 
-// todo
 app.get('/info', (request, response) => { 
     const currTime = new Date()
     Person.countDocuments({}, function (err, count) {
@@ -51,7 +50,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => { 
+app.post('/api/persons', (request, response, next) => { 
   const body = request.body 
 
   if (body.name === undefined) { 
@@ -63,9 +62,16 @@ app.post('/api/persons', (request, response) => {
     number: body.number 
   })
 
-  person.save().then(savedPerson => { 
-    response.json(savedPerson)
-  })
+  person
+    .save()
+    .then(savedPerson => savedPerson.toJSON())
+    .then(savedAndFormattedPerson => {
+        response.json(savedAndFormattedPerson)
+    })
+    .catch(error => {
+        console.log(error)
+        next(error)
+    })
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -78,7 +84,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 
     Person.findByIdAndUpdate(request.params.id, person, { new: true })
       .then(updatedPerson => {
-          response.json(updatedPerson)
+          response.json(updatedPerson.)
       })
       .catch(error => next(error))
 })
@@ -94,7 +100,9 @@ const errorHandler = (error, request, response, next) => {
   
   if (error.name === 'CastError' && error.kind == 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+      return response.status(400).send({ error: error.message })
+  }
   
   next(error)
 }
