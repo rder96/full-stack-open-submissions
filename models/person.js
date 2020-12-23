382387b4307059400eb8c3435d538a0227f58aed
rder96 <rder@wisc.edu>
2020-12-23 21:11:13 +0800

    done part3d

--- models/person.js
+++ models/person.js
@@ -5,35 +5,35 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
-})
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true, 
-    unique: true, 
-    minlength: 3, 
-  },  
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
   number: {
     type: String,
-    required: true, 
-    minlength: 8, 
-  }
-}) 
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => { 
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id      
-      delete returnedObject.__v
+    required: true,
+    minlength: 8,
   }
 })
 
-personSchema.plugin(uniqueValidator);
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+personSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Person', personSchema)
